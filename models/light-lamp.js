3e1982d26ed3ffa5f7f91464c3634501dfdfb733
NIKHIL JAIN <34137170+nikhiljainjain@users.noreply.github.com>
2019-07-23 05:54:20 +0530

    schema updation

--- models/light-lamp.js
+++ models/light-lamp.js
@@ -2,14 +2,14 @@ let mongoose = require("mongoose");
 let shortid = require("shortid");
 
 let idea = new mongoose.Schema({
-  name: { type: String, default: undefined, require: true },
+  name: { type: String, default: null, require: true },
   time: { type: Date, default: Date.now },
-  title: { type: String, default: undefined, require: true, unique: true },
-  desc: { type: String, default: undefined, require: true, unique: true },
-  club: { type: String, default: undefined },
+  title: { type: String, default: null},
+  desc: { type: String, default: null},
+  club: { type: String, default: null },
   ideaId: { type: String, default: shortid.generate },
-  email: { type: String, require: true },
-  long_desc: { type: String, default: undefined },
+  email: { type: String, default: null},
+  long_desc: { type: String, default: null},
   likes: [
     {
       type: String,
